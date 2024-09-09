0060c6bb1fa4b5bf75a360b27a1340404e70cef0
richard <richard80@orange.fr>
2024-09-09 07:13:18 +0000

    product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,16 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    /**
+     * @Route("/products", name="product_list")
+     */
+    public function listProducts()
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'title' => 'Liste des produits',
+            'content' => 'Ici, vous afficherez la liste de vos produits',
+        ]);
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_view")
+     */
+    public function viewProduct(Request $request, $id)
+    {
+        return $this->render('product/index.html.twig', [
+            'title' => 'Affichage du produit',
+            'content' => 'Vous affichez le produit avec l\'ID : ' . $id,
         ]);
     }
 }
